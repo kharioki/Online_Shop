42929ca1eb1dfa4f71b0ef08dee5881ec8850f0a
kharioki <antolisah@gmail.com>
2020-07-04 23:38:33 +0300

    project init

---
+++ README.md
@@ -0,0 +1,4 @@
+## Online_Shop
+
+A new Flutter project.
+An implementation of an online shop.
---
+++ android/app/src/main/kotlin/com/example/Online_Shop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.Online_Shop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
