106bba5133851d152208cc6ad7138fac999b2156
kharioki <antolisah@gmail.com>
2020-07-05 03:58:11 +0300

    appbar

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,40 @@
+import 'package:Online_Shop/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0.0,
+      leading: IconButton(
+        icon: SvgPicture.asset('assets/icons/back.svg'),
+        onPressed: () {},
+      ),
+      actions: [
+        IconButton(
+          icon: SvgPicture.asset(
+            'assets/icons/search.svg',
+            color: kTextColor,
+          ),
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: SvgPicture.asset(
+            'assets/icons/cart.svg',
+            color: kTextColor,
+          ),
+          onPressed: () {},
+        ),
+        SizedBox(width: kDefaultPaddin / 2),
+      ],
+    );
+  }
+}
